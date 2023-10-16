b9c8a409bb81c228d726c254ae549e7ad61e7c1b
Maruan Al-Shedivat <alshedivat@users.noreply.github.com>
2023-10-16 00:31:54 -0400

    Add optimal denoiser

--- diffusion/denoisers.py
+++ diffusion/denoisers.py
@@ -20,11 +20,6 @@ Tensor = torch.Tensor
 class Denoiser(abc.ABC, torch.nn.Module):
     """Abstract base class for denoisers."""
 
-    def __init__(self, model, sigma_data):
-        super().__init__()
-        self.model = model
-        self.sigma_data = sigma_data
-
     @abc.abstractmethod
     def forward(self, input, sigma, **kwargs):
         """Computes the denoised output for a given input and noise level.
@@ -35,6 +30,9 @@ class Denoiser(abc.ABC, torch.nn.Module):
 
 class SimpleDenoiser(Denoiser):
     """Simple denoiser that does not perform any preconditioning."""
+
+    def __init__(self, model):
+        self.model = model
 
     def forward(self, input, sigma, **kwargs):
         return self.model(input, sigma, **kwargs)
@@ -70,6 +68,11 @@ class KarrasDenoiser(Denoiser):
     Reference: https://arxiv.org/abs/2206.00364.
     """
 
+    def __init__(self, model, sigma_data):
+        super().__init__()
+        self.model = model
+        self.sigma_data = sigma_data
+
     def _c_skip(self, logsnr):
         return torch.sigmoid(logsnr)
 
@@ -85,3 +88,46 @@ class KarrasDenoiser(Denoiser):
         c_out = utils.expand_dims(self._c_out(logsnr), input.ndim)
         c_skip = utils.expand_dims(self._c_skip(logsnr), input.ndim)
         return c_skip * input + c_out * self.model(c_in * input, logsnr, **kwargs)
+
+
+class KarrasOptimalDenoiser(Denoiser):
+    """Optimal denoiser that analytically minimizes denoising loss on the training data.
+
+    The optimal denoiser depends on the training data and takes the following form:
+
+        D(y, sigma) = [sum_i y_i Gau(y; y_i, sigma**2)] / [sum_i Gau(y; y_i, sigma**2)],
+        where Gau(y; y_i, sigma) is Gaussian dencity with mean y_i and std sigma.
+
+    For details see Appendix B.3 in Karras et al. (2022).
+
+    Reference: https://arxiv.org/abs/2206.00364.
+    """
+
+    def __init__(self, train_data):
+        super().__init__()
+        self.train_data = train_data
+
+        # Compute sigma_data.
+        self.sigma_data = torch.std(self.train_data.tensors[0])
+
+    def _normal_prob(self, x, mu, sigma):
+        batch_size = x.shape[0]
+        prob_per_dim = torch.exp(-0.5 * ((x - mu) / sigma) ** 2) / (sigma * (2 * torch.pi) ** 0.5)
+        return torch.prod(prob_per_dim.view(batch_size, -1), dim=-1, keepdim=True)
+
+    @torch.inference_mode()
+    def forward(self, input, sigma, **kwargs):
+        del kwargs  # Unused.
+        sigma = utils.expand_dims(sigma, input.ndim)
+
+        # Iterate over training data and compute the optimal denoised output for the given sigma.
+        output = torch.zeros_like(input)
+        normalization = torch.zeros_like(sigma)
+        for y_i in self.train_data:
+            y_i = y_i.unsqueeze(0)
+            p_i = self._normal_prob(input, y_i, sigma)
+            output += y_i * p_i
+            normalization += p_i
+        output /= normalization
+
+        return output
